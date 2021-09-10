4473dc6ece2266a29e00709dc67b140e452a129a
ly0unsi <mcabdo25@gmail.com>
2021-09-10 02:41:18 +0100

    fc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap"
             rel="stylesheet"
           />
+
           <title>Abdellah Lyounsi</title>
         </Head>
         <body className="bg-fixed text-sm bg-gradient-to-r from-red-500 to-blue-500 dark:from-dark-500 dark:to-dark-700 dark:text-white">
