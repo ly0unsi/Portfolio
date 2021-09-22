5deda515cca76231a337539c3ebce2c3a5c5843f
ly0unsi <mcabdo25@gmail.com>
2021-09-22 19:31:47 +0100

    commit

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -64,6 +64,7 @@ const ProjectCard: FunctionComponent<{
                 {deployed_url!=="unvailable" &&
                     <a
                     href={deployed_url}
+                    target="_blank"
                     className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200"
                   >
                     
